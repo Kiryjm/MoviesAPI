9dce3799a9da5f08da20ca2286610f8e78bd0140
Kiryjm <kiryjm@gmail.com>
2020-12-10 19:56:02 +0300

    Warning logging level for whole application, loggers for service and controller were added.

--- MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/Controllers/GenresController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
 using MoviesAPI.Entities;
 using MoviesAPI.Services;
 
@@ -14,10 +15,12 @@ namespace MoviesAPI.Controllers
     public class GenresController : ControllerBase
     {
         private readonly IRepository repository;
+        private ILogger<GenresController> logger;
 
-        public GenresController(IRepository repository)
+        public GenresController(IRepository repository, ILogger<GenresController> logger)
         {
             this.repository = repository;
+            this.logger = logger;
         }
 
         [HttpGet] //api/genres
@@ -26,15 +29,20 @@ namespace MoviesAPI.Controllers
         //api/genres
         public async Task<ActionResult<List<Genre>>> Get()
         {
+            logger.LogInformation("Getting all the genres");
             return await repository.GetAllGenres();
         }
 
         [HttpGet("{Id:int}", Name = "getGenre")] //api/genres/1
         public ActionResult<Genre> Get(int Id, string param2)
         {
+            logger.LogDebug("Get by Id method executing...");
             var genre = repository.GetGenreById(Id);
             if (genre == null)
             {
+                logger.LogWarning($"Genre with Id {Id} not found");
+                logger.LogError("This is an error");
+
                 return NotFound();
             }
             return genre;
--- MoviesAPI/Services/InMemoryRepository.cs
+++ MoviesAPI/Services/InMemoryRepository.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using MoviesAPI.Entities;
 
 namespace MoviesAPI.Services
 {
     public class InMemoryRepository : IRepository
     {
+        private readonly ILogger<InMemoryRepository> logger;
         private List<Genre> _genres;
-        public InMemoryRepository()
+        public InMemoryRepository(ILogger<InMemoryRepository> logger)
         {
             _genres = new List<Genre>()
             {
                 new Genre(){ Id = 1, Name = "Comedy"},
                 new Genre(){ Id = 2, Name = "Action"},
             };
+            this.logger = logger;
+
         }
         public async Task<List<Genre>> GetAllGenres()
         {
+            logger.LogInformation("Executing GetAllGenres");
             await Task.Delay(1);
             return _genres;
         }
